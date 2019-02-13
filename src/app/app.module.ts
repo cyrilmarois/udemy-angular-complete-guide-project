50a1bab1be9b00858eaeb8cb0d9002ff8036b1dc
Cyril Marois <cyrilmarois@gmail.com>
2019-02-13 22:45:55 +0100

    Feat (Structure): Create components basic structure
    Add header, recipe, recipe-list,recipe-detail, shopping and shopping-edit components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ShoppingComponent } from './components/shopping/shopping.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { RecipeDetailComponent } from './components/recipe/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './components/recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipe/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './components/shopping/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingComponent,
+    RecipeComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
