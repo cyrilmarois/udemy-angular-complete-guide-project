14b6c195cf9a2c3a48a8befba9a061665e1e1881
Cyril Marois <cyrilmarois@gmail.com>
2019-02-24 00:16:44 +0100

    Feat (Shopping): Add shopping component with ingredient model

--- src/app/components/shopping/shopping.component.ts
+++ src/app/components/shopping/shopping.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from './ingredient.model';
+
 @Component({
   selector: 'app-shopping',
   templateUrl: './shopping.component.html',
   styleUrls: ['./shopping.component.sass']
 })
 export class ShoppingComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('chia', '1/4 cup'),
+    new Ingredient('coco milk', '1 cup'),
+  ];
 
   constructor() { }
 
