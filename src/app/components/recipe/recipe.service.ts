d8c859a69b60abb1a77c8f923f69f47d112a2f41
Cyril Marois <cyrilmarois@gmail.com>
2019-10-24 00:36:57 +0200

    Style(app): enhance app style
    add header background-color
    replace deprecated pull-right/left by float-left/right
    update content wording
    update link color

--- src/app/components/recipe/recipe.service.ts
+++ src/app/components/recipe/recipe.service.ts
@@ -1,20 +1,29 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import { Recipe } from './recipe.model';
-import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import {
+  Injectable,
+  EventEmitter
+} from '@angular/core';
+import {
+  Recipe
+} from './recipe.model';
+import {
+  Ingredient
+} from '../shared/ingredient.model';
+import {
+  ShoppingListService
+} from '../shopping-list/shopping-list.service';
 @Injectable({
   providedIn: 'root'
 })
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter < Recipe > ();
 
   private _recipes: Recipe[] = [
     new Recipe(
-      'pudding coco chia', 
-      'chia pudding coco', 
-      'https://c.pxhere.com/photos/00/41/porridge_muesli_breakfast_vegan_oatmeal_chia_cocoa_nibs_orange-1277714.jpg!d', 
+      'pudding coco chia',
+      'chia pudding coco',
+      'https://c.pxhere.com/photos/00/41/porridge_muesli_breakfast_vegan_oatmeal_chia_cocoa_nibs_orange-1277714.jpg!d',
       [
         new Ingredient('coco milk', '1 cup'),
         new Ingredient('chia', '1/4 cup'),
@@ -22,24 +31,22 @@ export class RecipeService {
         new Ingredient('maple syrup', '1 tsp'),
         new Ingredient('coco powder', '1/4 cup')
       ]),
-    new Recipe('pudding Matcha chia', 
-    'chia pudding cMatchaoco', 
-    'https://c.pxhere.com/photos/00/41/porridge_muesli_breakfast_vegan_oatmeal_chia_cocoa_nibs_orange-1277714.jpg!d', 
-    [
-      new Ingredient('coco milk', '1 cup'),
-      new Ingredient('chia', '1/4 cup'),
-      new Ingredient('vanilla', '1 tbsp'),
-      new Ingredient('maple syrup', '1 tsp'),
-      new Ingredient('Matcha', '1/4 cup')
-    ])
+    new Recipe('pudding matcha chia',
+      'chia pudding matcha',
+      'https://c.pxhere.com/photos/00/41/porridge_muesli_breakfast_vegan_oatmeal_chia_cocoa_nibs_orange-1277714.jpg!d',
+      [
+        new Ingredient('coco milk', '1 cup'),
+        new Ingredient('chia', '1/4 cup'),
+        new Ingredient('vanilla', '1 tbsp'),
+        new Ingredient('maple syrup', '1 tsp'),
+        new Ingredient('Matcha', '1/4 cup')
+      ])
   ];
 
-  constructor(private _shoppingListService: ShoppingListService) {    
-
-  }
+  constructor(private _shoppingListService: ShoppingListService) {}
 
   getRecipes() {
-    return this._recipes.slice();  // use slice with empty argument to return a copy of recipes
+    return this._recipes.slice(); // use slice with empty argument to return a copy of recipes
   }
 
   addIngredientToShoppingList(ingredients: Ingredient[]) {
