7a29a11edece354879c826a3926ee99d79283212
Cyril Marois <cyrilmarois@gmail.com>
2019-12-26 15:04:00 +0100

    Feat(project): remove component folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,17 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
-import { HeaderComponent } from './components/header/header.component';
-import { ShoppingComponent } from './components/shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/shopping-edit.component';
-import { ShoppingListService } from './components/shopping-list/shopping-list.service';
-import { RecipeComponent } from './components/recipe/recipe.component';
-import { RecipeDetailComponent } from './components/recipe/recipe-detail/recipe-detail.component';
-import { RecipeListComponent } from './components/recipe/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './components/recipe/recipe-list/recipe-item/recipe-item.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
 
-import { DropdownDirective } from './components/shared/dropdown.directive';
+import { DropdownDirective } from './shared/dropdown.directive';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { DropdownDirective } from './components/shared/dropdown.directive';
     RecipeListComponent,
     RecipeItemComponent,
     ShoppingEditComponent,
-    DropdownDirective
+    DropdownDirective,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
