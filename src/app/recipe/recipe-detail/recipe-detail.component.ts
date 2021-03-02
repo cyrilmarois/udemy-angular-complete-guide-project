5a999dcc0b20aa36624c525c848ac229403be6e8
cyrilmarois <cyrilmarois@gmail.com>
2021-03-02 23:03:24 +0100

    feat(recipe): add recipe managment

--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -1,27 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit, Input } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
-import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recipe.service';
+import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.sass']
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.component.html",
+  styleUrls: ["./recipe-detail.component.sass"],
 })
 export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private _recipeService: RecipeService,
-    private _route: ActivatedRoute) { }
+  constructor(
+    private _recipeService: RecipeService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) {}
 
   ngOnInit() {
-    this._route.params
-      .subscribe((params: Params) => {
-        this.id = +params['id'];
-        this.recipe = this._recipeService.getRecipe(this.id);
-      });
+    this._route.params.subscribe((params: Params) => {
+      console.log([params]);
+      this.id = +params["id"];
+      this.recipe = this._recipeService.getRecipe(this.id);
+    });
     console.log(this.recipe);
   }
 
@@ -29,4 +32,9 @@ export class RecipeDetailComponent implements OnInit {
     this._recipeService.addIngredientToShoppingList(this.recipe.ingredients);
   }
 
+  onEditRecipe() {
+    this._router.navigate(["../", this.id, "edit"], {
+      relativeTo: this._route,
+    });
+  }
 }
--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,20 +1,10 @@
-import {
-  Injectable,
-  EventEmitter
-} from '@angular/core';
-import {
-  Recipe
-} from './recipe.model';
-import {
-  Ingredient
-} from '../shared/ingredient.model';
-import {
-  ShoppingListService
-} from '../shopping-list/shopping-list.service';
+import { Injectable, EventEmitter } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-
 @Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -22,34 +12,36 @@ export class RecipeService {
   private _recipes: Recipe[] = [
     new Recipe(
       1,
-      'pudding coco chia',
-      'chia pudding coco',
-      'https://c.pxhere.com/photos/00/41/porridge_muesli_breakfast_vegan_oatmeal_chia_cocoa_nibs_orange-1277714.jpg!d',
+      "pudding coco chia",
+      "chia pudding coco",
+      "https://c.pxhere.com/photos/00/41/porridge_muesli_breakfast_vegan_oatmeal_chia_cocoa_nibs_orange-1277714.jpg!d",
       [
-        new Ingredient('coco milk', '1 cup'),
-        new Ingredient('chia', '1/4 cup'),
-        new Ingredient('vanilla', '1 tbsp'),
-        new Ingredient('maple syrup', '1 tsp'),
-        new Ingredient('coco powder', '1/4 cup')
-      ]),
+        new Ingredient("coco milk", "1 cup"),
+        new Ingredient("chia", "1/4 cup"),
+        new Ingredient("vanilla", "1 tbsp"),
+        new Ingredient("maple syrup", "1 tsp"),
+        new Ingredient("coco powder", "1/4 cup"),
+      ]
+    ),
     new Recipe(
       2,
-      'pudding matcha chia',
-      'chia pudding matcha',
-      'https://c.pxhere.com/photos/00/41/porridge_muesli_breakfast_vegan_oatmeal_chia_cocoa_nibs_orange-1277714.jpg!d',
+      "pudding matcha chia",
+      "chia pudding matcha",
+      "https://c.pxhere.com/photos/00/41/porridge_muesli_breakfast_vegan_oatmeal_chia_cocoa_nibs_orange-1277714.jpg!d",
       [
-        new Ingredient('coco milk', '1 cup'),
-        new Ingredient('chia', '1/4 cup'),
-        new Ingredient('vanilla', '1 tbsp'),
-        new Ingredient('maple syrup', '1 tsp'),
-        new Ingredient('Matcha', '1/4 cup')
-      ])
+        new Ingredient("coco milk", "1 cup"),
+        new Ingredient("chia", "1/4 cup"),
+        new Ingredient("vanilla", "1 tbsp"),
+        new Ingredient("maple syrup", "1 tsp"),
+        new Ingredient("Matcha", "1/4 cup"),
+      ]
+    ),
   ];
 
   constructor(private _shoppingListService: ShoppingListService) {}
 
   getRecipes() {
-    return this._recipes.slice(); // use slice with empty argument to return a copy of recipes
+    return this._recipes.slice(); // slice with empty argument return a copy of recipes
   }
 
   getRecipe(id: number): Recipe {
